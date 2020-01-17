c4f9a4de93428dbf76636b70141393ade9c79657
RebeccaEvans <evans.rebeccaj@gmail.com>
2020-01-16 22:43:55 -0800

    added routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,47 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 import './App.css';
 
+import About from './About'
+import Blog from './Blog'
+import Projects from './Projects'
+
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-      <h1>Portfolio</h1>
-      </header>
-    </div>
+    <Router>
+      <div className='App'>
+        <header className='App-header'>
+          <div>
+            <ul>
+              <li>
+                <Link to='/'>home</Link>
+              </li>
+              <li>
+                <Link to='/about'>about</Link>
+              </li>
+              <li>
+                <Link to='/blog'>blog</Link>
+              </li>
+              <li>
+                <Link to='/projects'>projects</Link>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h1>Portfolio</h1>
+          </div>
+        </header>
+
+        <Switch>
+          <Route exact path='/' component={App} />
+          <Route path='/about' component={About} />
+          <Route path='/blog' component={Blog} />
+          <Route path='/projects' component={Projects} />
+        </Switch>
+        
+      </div>
+    </Router>
   );
 }
 
