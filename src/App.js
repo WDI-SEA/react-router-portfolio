dac14a8843e427ec4543e3cc1fb5118fd9ddd226
Weston Bailey <void@MBP.local>
2020-07-17 17:45:18 -0700

    project base

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Navbar from './NavBar';
+import Home from './Home';
+import Blog from './Blog';
+import Projects from './Projects';
+import About from './About';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
