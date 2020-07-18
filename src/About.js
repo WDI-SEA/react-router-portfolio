054ff9d215461eac5aa3f2231c973d192a07c5f6
zbarovsky <zachary.barovsky@gmail.com>
2020-07-18 15:07:33 -0700

    grammatical corrections

--- src/About.js
+++ src/About.js
@@ -3,7 +3,7 @@ import React from 'react'
 function About(props) {
     return (
         <div>
-            <h2 className="header">Want to hire me? Why wouldn't you with all of the following qualiifications.</h2>
+            <h2 className="header">Want to hire me? Why wouldn't you with all of the following qualifications.</h2>
             <ul>
                 {props.skills.map((skill) => <li className="skillList" key={skill}>{skill}</li>)}
             </ul>
