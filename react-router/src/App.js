17d6790e9be0913cce14197392677906d6fa8849
d4vves <d4vves@gmail.com>
2020-07-19 12:09:46 -0500

    styling completed

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -5,49 +5,23 @@ import Home from './Components/Home'
 import Blog from './Components/Blog'
 import About from './Components/About'
 import Projects from './Components/Projects'
+import BlogData from './api/BlogData'
+import ProjectData from './api/ProjectData'
 
 function App() {
-  const [blogPosts, setBlogPosts] = useState([
-    {
-      title: 'CSS Tips and Tricks',
-      content: 'CSS is an incredibly powerful language! I personally love CSS, but some may get tripped up by its intricacies. If you are into CSS and want to learn some cool stuff, I would reccomend css-tricks.com',
-      date: '7/19/2020'
-    },
-    {
-      title: 'My New Favorite Code Editor',
-      content: 'I have used repl.it. I have used codepen. There are so many options for web based editors, but my new favorite has got to be codesandbox.io. Check it out!',
-      date: '7/17/2020'
-    },
-    {
-      title: 'Country Music Rules',
-      content: 'Country gets a bad wrap. But if you look deeper into the scene, specifically the origins of country, it is incredible! Check out Waylon Jennings, Bobby Bare, The Louvin Brothers, Johnny Western, Glen Campbell, Eddie Noack and the like for a taste of the real stuff!',
-      date: '7/17/2020'
-    }
-  ])
+  const [blogPosts, setBlogPosts] = useState(BlogData)
 
-  const [projects, setProjects] = useState([
-    {
-      title: 'Ani-Cal',
-      image: 'https://raw.githubusercontent.com/d4vves/anime-scheduler/master/public/img/screenshot.png',
-      content: 'Ani-Cal is my first full-stack web app that I created in my cohort. Here you can plan out your schedule for the upcoming anime season! How fun.',
-      link: 'https://github.com/d4vves/anime-scheduler'
-    },
-    {
-      title: 'Garbage Boy',
-      image: 'https://raw.githubusercontent.com/d4vves/garbage-boy/master/img/screen-shot.png',
-      content: 'GARBAGE BOY! For my first project in my cohort I was to create a game using HTML, CSS and JavaScript. The result was the legendary Garbage Boy.',
-      link: 'https://github.com/d4vves/garbage-boy'
-    }
-  ])
+  const [projects, setProjects] = useState(ProjectData)
 
   return (
     <Router>
       <div>
         <nav>
-          <Link to='/'>Home</Link>
-          <Link to='/blog'>Blog</Link>
-          <Link to='/about'>About</Link>
-          <Link to='/projects'>Projects</Link>
+          <h2>davves //</h2>
+          <Link to='/'>home</Link>
+          <Link to='/blog'>blog</Link>
+          <Link to='/about'>about</Link>
+          <Link to='/projects'>projects</Link>
         </nav>
       </div>
       <div className='App'>
---
+++ react-router/src/api/ProjectData.js
@@ -0,0 +1,18 @@
+import Project from "../Components/Project"
+
+const ProjectData = [
+    {
+      title: 'Ani-Cal',
+      image: 'https://raw.githubusercontent.com/d4vves/anime-scheduler/master/public/img/screenshot.png',
+      content: 'Ani-Cal is my first full-stack web app that I created in my cohort. Here you can plan out your schedule for the upcoming anime season! How fun.',
+      link: 'https://github.com/d4vves/anime-scheduler'
+    },
+    {
+      title: 'Garbage Boy',
+      image: 'https://raw.githubusercontent.com/d4vves/garbage-boy/master/img/screen-shot.png',
+      content: 'GARBAGE BOY! For my first project in my cohort I was to create a game using HTML, CSS and JavaScript. The result was the legendary Garbage Boy.',
+      link: 'https://github.com/d4vves/garbage-boy'
+    }
+  ]
+
+export default ProjectData
