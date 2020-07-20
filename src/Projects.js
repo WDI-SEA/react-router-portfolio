693e6cc1206fba564400e826fbef0a54b7719fe2
Nick Hvattum <nhvattum@gmail.com>
2020-07-20 08:55:01 -0700

    done

--- src/Projects.js
+++ src/Projects.js
@@ -3,7 +3,8 @@ import React from 'react'
 const Projects = (props) => {
     return (
         <div>
-            
+            <h2>My Projects:</h2>
+            <h4></h4>
         </div>
     )
 }
