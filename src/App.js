030f1a425fb1bdeba0b1d5dbd8a2ee6caaff6445
Joshua Danao <josh@Joshuas-MacBook-Air.local>
2020-07-19 19:22:29 -0700

    added some cool stuff

--- src/App.js
+++ src/App.js
@@ -11,27 +11,35 @@ function App() {
   let blogs = [
     { 
       name: 'Salman', 
-      about: "Likes to wake up people when they're sleeping." 
+      about: "Likes to wake up people when they're sleeping.", 
+      image: "https://scontent-sea1-1.xx.fbcdn.net/v/t1.0-9/22228641_10213074701375363_901385773467128542_n.jpg?_nc_cat=108&_nc_sid=85a577&_nc_ohc=e203fSi0YfQAX9MuAV2&_nc_ht=scontent-sea1-1.xx&oh=45e520522f22387e2ad0b3b3cedae4c0&oe=5F3AAB69"
     },
     { 
       name: 'Nick', 
-      about: "Super nice guy probably the nicest in the group" 
+      about: "Super nice guy probably the nicest in the group",
+      image: "https://ca.slack-edge.com/T0351JZQ0-U0141JELV4Y-710a221b9c63-512"
     },
     { 
       name: 'Nicholas', 
-      about: "He's the coolest" 
+      about: "He's the coolest",
+      image: "https://ca.slack-edge.com/T0351JZQ0-U013HUG5QHJ-0edcbb728e22-512"
     },
     { 
       name: 'Charles', 
-      about: "Youngest of the group, kinda clumsy" 
+      about: "Youngest of the group, kinda clumsy", 
+      image: "https://ca.slack-edge.com/T0351JZQ0-U012X1DNF2T-3950e7b26202-512"
     },
     { 
       name: 'Scott', 
-      about: "Loves plants and cats" 
+      about: "Loves plants and cats", 
+      image: "https://ca.slack-edge.com/T0351JZQ0-UT9HS81T7-5fff524dcf3e-512"
     },
     { 
       name: 'Zack', 
-      about: "Very good at CSS, would like to get advice from him." },
+      about: "Very good at CSS, would like to get advice from him some day.",
+      image: "https://ca.slack-edge.com/T0351JZQ0-UV0CS3TQF-6394ecdb9322-512" 
+    }
+
   ]
 
   return (
--- src/Blog.js
+++ src/Blog.js
@@ -3,18 +3,19 @@ import React from 'react';
 const Blog = (props) => {
   return (
     <div className="blog-container">
-      <div className="ul-container">
-        <ul>
+        <ul className="ul">
+        <div className="ul-container">
           {props.contents.map(content =>
           <div className="li-container">
             <li className="blog-list">
               <h1>{content.name}</h1>
+              <img className="blog-pic" src={content.image} />
               <p>{content.about}</p>
             </li>
           </div>
           )}
+        </div>
         </ul>
-      </div>
     </div>
   )
 }
