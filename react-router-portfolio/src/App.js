d953f831893109eaf6a7b253eaea6f9ee8ca98c0
Caitlin Smith <caitlinelizabeth888@gmail.com>
2020-07-20 09:18:31 -0700

    fixed blogs

--- react-router-portfolio/src/App.js
+++ react-router-portfolio/src/App.js
@@ -19,29 +19,30 @@ function App() {
       date: "7/19/20"
     }
   ])
-  setBlogPosts()
+  // setBlogPosts()
   return (
-  <Router>
-    <div>
-    <link href="https://fonts.googleapis.com/css2?family=Share+Tech&display=swap" rel="stylesheet" /
-    >
-      <nav>
-        <Link to='/' className="nav">Home</Link>
-        <Link to="/Blog" className="nav">Blog</Link>
-        <Link to="/About" className="nav">About</Link>
-        <Link to="/Projects" className="nav">Projects</Link>
-      </nav>
-      <Blog blogPosts={blogPosts} />
-    </div>
-    <div className="App">
-      <h1 className="title">Welcome to Caitlin Smith's Porfolio</h1>
-      <Route path='/' component={Home} />
-      <Route path='/Blog' component={Blog} />
-      <Route path='/About' component={About} />
-      <Route path='/Projects' component={Projects} />
-    </div>
-  </Router>
+    <Router>
+      <div>
+      <link href="https://fonts.googleapis.com/css2?family=Share+Tech&display=swap" rel="stylesheet" /
+      >
+        <nav>
+          <Link to='/' className="nav">Home</Link>
+          <Link to="/Blog" className="nav">Blog</Link>
+          <Link to="/About" className="nav">About</Link>
+          <Link to="/Projects" className="nav">Projects</Link>
+        </nav>
+        <Blog blogPosts={blogPosts} />
+      </div>
+      <div className="App">
+        <h1 className="title">Welcome to Caitlin Smith's Porfolio</h1>
+        <Route path='/' component={Home} />
+        <Route path='/Blog' component={Blog} />
+        <Route path='/About' component={About} />
+        <Route path='/Projects' component={Projects} />
+      </div>
+    </Router>
   );
 }
+
 
 export default App;
