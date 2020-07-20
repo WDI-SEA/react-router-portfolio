4052a6cf322a79c1a27ccff14e40c5bc6afff4d5
Jameson Aardalen <jamesonaardalen@Jamesons-MBP.attlocal.net>
2020-07-19 23:50:33 -0700

    setup react router, created header with navbar, created Home component

---
+++ router-portfolio/src/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="container center-align">
+      <h1>Jameson Aardalen</h1>
+      <h3>Full-Stack Web Developer</h3>
+    </div>
+  );
+};
+
+export default Home;
---
+++ router-portfolio/src/index.js
@@ -0,0 +1,55 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Home from "./Home";
+
+const App = () => {
+  return (
+    <Router>
+      <nav>
+        <div className="container">
+          <div className="nav-wrapper">
+            <a href="#" className="brand-logo">
+              Jamo's Portfolio
+            </a>
+            <ul id="nav-mobile" className="right hide-on-med-and-down">
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/projects">Projects</Link>
+              </li>
+              <li>
+                <Link to="/contact">Contact</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <div>
+        <Route exact path="/" component={Home} />
+      </div>
+      {/* <div>
+        <nav>
+          <Link to="/">Go Home!</Link>
+          <Link to="/contact">Contact Us!</Link>
+          <Link to="/procedures">Procedures</Link>
+        </nav>
+      </div>
+      <div className="App">
+        <h1>Dentist Info! </h1>
+        <Route exact path="/" component={Home} />
+        <Route path="/contact" component={Contact} />
+        <Route
+          path="/procedures"
+          render={() => <Procedures procedures={procedures} />}
+        />
+      </div> */}
+    </Router>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
