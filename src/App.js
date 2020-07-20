d85f38861427862cc6b7163e2a22f86a5442ba3a
Neri Ramirez <neriram@Neris-MacBook-Air.local>
2020-07-20 15:53:29 -0700

    state added

--- src/App.js
+++ src/App.js
@@ -8,9 +8,10 @@ import Homepage from './components/Homepage'
 import Blog from './components/Blog'
 import About from './components/About'
 import Projects from './components/Projects'
+import Navbar from 'react-bootstrap/Navbar'
 
 function App() {
-  let posts = [
+  let [posts, setPosts] = useState([
     {
     title: "My Coding Journey Begins",
     date: "5/26/2020",
@@ -21,16 +22,19 @@ function App() {
     date: "7/14/2020",
     content: "This new framework has destructured everything I knew"
   }
-  ]
+  ])
   return (
     <Router>
       <div>
+        <Container>
+          <Row>
         <nav>
           <Link to="/">Home</Link>{' '}
           <Link to="/blog">My Blog</Link>{' '}
           <Link to="/about">About Me</Link>{' '}
           <Link to="/projects">My Projects</Link>
         </nav>
+        </Navbar>
         <Route exact path="/" component={Homepage} />
         <Route path="/blog" render={() => <Blog posts={posts} />} />
         <Route path="/about" component={About} />
