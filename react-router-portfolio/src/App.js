d5fe7dafa0b4a884b6e1be8fad507c48300fe644
Caitlin Smith <caitlinelizabeth888@gmail.com>
2020-07-20 11:55:41 -0700

    finally figured out that I was not rendering the blog page correctly and it was not a use state issue

--- react-router-portfolio/src/App.js
+++ react-router-portfolio/src/App.js
@@ -6,8 +6,9 @@ import Blog from './Blog';
 import About from './About';
 import Projects from './Projects';
 
+
 function App() {
-  const [blogPosts, setBlogPosts] = useState([
+  const [blogPosts] = useState([
     {
       title: "Learning to Code in 12 Weeks",
       content: "While washing the dishes last night I was reflecting on the fact that 8 weeks ago I had no idea what was going on inside my computer. Today, I have created a web application game, a full stack menu creating application and I am learning REACT.",
@@ -18,8 +19,8 @@ function App() {
       content: "Learning code has introduced me to a whole new way of using my brain, now I am just trying to figure out how to combine my background in education and my new knowledge of coding.",
       date: "7/19/20"
     }
-  ])
-  // setBlogPosts()
+  ]);
+  
   return (
     <Router>
       <div>
@@ -31,14 +32,13 @@ function App() {
           <Link to="/About" className="nav">About</Link>
           <Link to="/Projects" className="nav">Projects</Link>
         </nav>
-        <Blog blogPosts={blogPosts} />
       </div>
       <div className="App">
         <h1 className="title">Welcome to Caitlin Smith's Porfolio</h1>
-        <Route path='/' component={Home} />
-        <Route path='/Blog' component={Blog} />
-        <Route path='/About' component={About} />
-        <Route path='/Projects' component={Projects} />
+        <Route path='/' render={Home} />
+        <Route path='/Blog' render={() => <Blog blogPosts= {blogPosts} />} />
+        <Route path='/About' render={About} />
+        <Route path='/Projects' render={Projects} />
       </div>
     </Router>
   );
--- react-router-portfolio/src/Blog.js
+++ react-router-portfolio/src/Blog.js
@@ -1,20 +1,20 @@
 import React from 'react';
 
 const Blog = (props) => {
-    return (
+   return (
       <div>
         <h1>Blog</h1>
           <ul>
-          {props.blogPosts.map(post => (
+          {props.blogPosts.map((post, index) => (
             <li>
-            <h2 key={post}>{post.title}</h2>
-            <h4 key={post}>{post.date}</h4>
-            <p key={post}>{post.content}</p>
+            <h2 key={index}>{post.title}</h2>
+            <h4 key={index}>{post.date}</h4>
+            <p key={index}>{post.content}</p>
             </li>
           ))}
           </ul>
       </div>
     )
-  }
+          }
 
 export default Blog;
