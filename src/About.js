6d92067d61d88b1484ba9f312adeee696a0c1fbe
Salman Malik <salmanmalik@Salmans-Air.attlocal.net>
2020-07-21 02:47:24 -0500

    Pictures are functional now, issues with blog page have been resolved, CSS added for stretch goal

--- src/About.js
+++ src/About.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import ProfilePic from './pics/slack profile pic.jpg'
+
 
 const About = () => {
     return(
         <div className="aboutpage">
             <h1> About Me: </h1>
 
-            <img src= "https://i.imgur.com/I1owp56.jpg" alt=" me"/>
+            <img src={ProfilePic} alt=" me" width="250" height="350" />
             <p> I'm a developer who went through the General Assembly Software Engineering Immersive program! I have experience in:</p>
             <ul>
                 <li>HTML, CSS, SAAS</li>
--- src/Projects.js
+++ src/Projects.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from 'react';
+import Marvel1 from './pics/Marvel1.png';
+import Marvel2 from './pics/Marvel2.png';
+import Memory1 from './pics/Memory1.png';
+import Memory2 from './pics/Memory2.png';
+
 
 const Projects = () => {
     return (
         <div className = "projectpage">
             <h1> Check out my work!</h1>
+            <div className="p2">
+                <p> <a href="https://github.com/salman5436/P2-Marvel">Marvel Pick Em'</a>: This was my project in General Assembly that implmented Node, Express, Javascript, SQL, CSS. and more! </p>
+                <img src={Marvel1} height="500" width="700" alt="marvel pick em website screenshot that I created using Node, Express, SQl, Javascript, and CSS" />
+                <img src={Marvel2} height="490" width="700" alt="marvel pick em website screenshot that I created using Node, Express, SQl, Javascript, and CSS" />
+            </div>
             <div className="p1">
-                <h3>This was my project for</h3>
+                <p> <a href="https://github.com/salman5436/Memory-Matching-Bonanza">The Memory Matching Bonanza</a>: This was my project in General Assembly that implmented Node, Express, Javascript, SQL, CSS. and more! </p>
+                <img src={Memory1} height="500" width="700" alt="Memory Matching card game screenshot that I created using Javascript, CSS, HTML, and more" />
+                <img src={Memory2} height="490" width="700" alt="Memory Matching card game screenshot that I created using Javascript, CSS, HTML, and more" />
             </div>
         </div>
     )
