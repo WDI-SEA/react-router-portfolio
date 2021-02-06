c61d71e45337fca2afd7aa604b6f44870aa87e13
austintip <austin.d.tipograph@gmail.com>
2021-02-06 15:12:03 -0500

    home pahe is routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from './component/Home'
+
+class App extends Component {
+  render(){
+    return (
+      <Router>
+        <main className="App">
+          <Route exact path="/" component={Home} />
+        </main>
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/component/Home.js
@@ -0,0 +1,13 @@
+import React, { Component} from 'react'
+
+class Home extends Component{
+    render(){
+        return(
+            <div className="homepage">
+                <h1>Hello! Welcome to my Portfolio</h1>
+            </div>
+        )
+    }
+}
+
+export default Home
