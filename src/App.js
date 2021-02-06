b06412a3dd5dd7040a3693d4dd5134f49dc160fd
kys3923 <kys3923@gmail.com>
2021-02-05 23:59:06 -0500

    started portfolio building

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import './App.css'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>Header here</h1>
+        <h1>Home here</h1>
+      </div>
+    )
+  }
+}
+
+export default App
