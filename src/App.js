e02d1e8132b7f29f5a8325df14aac784bc620b0a
ChassyAHustla <chassyahustla@ChassyAHustlas-MacBook-Pro.local>
2021-02-07 20:17:16 -0800

    updated

--- src/App.js
+++ src/App.js
@@ -10,14 +10,14 @@ import Header from './components/partials/Header'
 import Widget from './components/pages/Widget'
 
 class App extends Component {
-  render() {
-    let posts = {
-      state: {
-        title: '',
-        body: ''
-      }
-    }
+  // state = {
+  //   posts: [
+  //   {title: 'Blog #1', body: 'Here is Blog #1'},
+  //   {title: 'Blog #2', body: 'Here is Blog #2'}
+  //   ]
+  // }
 
+  render() {
 
     return (
       <Router>
@@ -26,8 +26,9 @@ class App extends Component {
           <Route exact path="/" component={Home} />
           <Route path="/projects" component={Projects} />
           <Route path="/about" component={About} />
-          <Route path="/blog" render={() => <Blog blog={posts} /> } />
-          <Route path="/widget/:id" render={(props) => <Widget {...props} blog={posts}/>} />
+          <Route path="/blog" component={Blog} />
+          {/* <Route path="/blog" render={() => <Blog blog={this.state.posts.title} /> } />
+          <Route path="/widget/:id" render={(props) => <Widget {...props} blog={this.state.posts}/>} /> */}
         </div>
       </Router>
     )
--- src/components/pages/Blog.js
+++ src/components/pages/Blog.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 
 class Blog extends Component {
     render() {
         return (
             <div>
+                <h3>Recent Blog Posts</h3>
                 {/* <ul>
                     {this.props.blog.map((blog, i) => {
                         return <li key={`key is ${i}`}><Link to={`/widget/${i}`}>{blog}</Link></li>
