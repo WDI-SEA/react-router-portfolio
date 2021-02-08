d2e997b385806d54dd01ec3b2487af5fe8f34176
Blake DeGraw <bbdegraw@gmail.com>
2021-02-08 10:21:43 -0800

    updated 'about' page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,8 +4,9 @@ class About extends Component {
     render() {
         return(
             <div>
-                <p>My primary background is in music composition and audio production. After years of working with various recording and video editing programs, I recently decided to try my hand at video game development, as I figure much of my skills are transferrable.</p>
+                <p>My primary background is in music composition and audio production. After years of working with various sound and video editing programs, I recently decided to try my hand at video game development, as I figure much of my skillset is transferrable.</p>
                 <p>I currently work as a one-man dev team, doing all the programming, asset design, sound design, level design, and character animations myself. It is exhausting work, but fun!</p>
+                <p>A concept I like to explore in my games is the relationship between the player and the character they control, as well as the nature of escapism itself. A recurring theme in my games is self-awareness: many of my game's characters know or suspect that they are characters in a video game, and revere the player as a sort of demi-god. By giving the characters self-awareness, I feel that the player must make a conscious effort to become invested in the plot, rather than just going along with it because...it's just there.</p>
             </div>
         )
     }
