36a39117c20ffdf986806f52f92e88c6e856c675
damatoaj <mr.arthurdamato@gmail.com>
2021-02-09 10:13:48 -0800

    fixed image paths

--- src/Components/Partials/Footer.js
+++ src/Components/Partials/Footer.js
@@ -10,10 +10,10 @@ class Footer extends Component {
                     <h6>Seattle, Washington</h6>
                     <h6>Email: <a href="mailto:mr.arthurdamato@gmail.com">mr.arthurdamato@gmail.com</a></h6>
                 </address>
-                    <a href="https://github.com/damatoaj" className="socialMedia"><img src="./GitHub-Logo.png" /></a>
-                    <a href="https://www.instagram.com/thundrousprophet/?hl=en"  className="socialMedia"><img  src="./images/instagram_icon.svg" /></a>
-                    <a href="https://www.linkedin.com/in/arthur-d-amato-45b6b169/" className="socialMedia"><img  src="./images/linked_in_logo.svg" /></a>
-                    <a href="https://arthurdamato.wordpress.com/" className="socialMedia"><img src="./images/wordpress_logo.svg" /></a>
+                    <a href="https://github.com/damatoaj" className="socialMedia"><img src="/images/GitHub-Logo.png" /></a>
+                    <a href="https://www.instagram.com/thundrousprophet/?hl=en"  className="socialMedia"><img  src="/images/instagram_icon.svg" /></a>
+                    <a href="https://www.linkedin.com/in/arthur-d-amato-45b6b169/" className="socialMedia"><img  src="/images/linked_in_logo.svg" /></a>
+                    <a href="https://arthurdamato.wordpress.com/" className="socialMedia"><img src="/images/wordpress_logo.svg" /></a>
             </footer>
         )
     }
--- src/Components/Statics/MyProjects.js
+++ src/Components/Statics/MyProjects.js
@@ -3,13 +3,13 @@ const MyProjects = [
         id: 1,
         title: 'What\'cha Makin\'?', 
         synopsis: '',
-        image: './images/Lunch.png'
+        image: '/images/Lunch.png'
     },
     {
         id: 2,
         title: 'Hnefetafl', 
         synopsis: '',
-        image: './images/hnefetafl.png'
+        image: '/images/hnefetafl.png'
     },
 ]
 
