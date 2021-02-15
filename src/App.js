ffaa603854454b09897015e813774d47fbf6f7ea
Jeffrey Luzon <jeffreyluzon@Jeffreys-MacBook-Air.local>
2021-02-14 19:27:03 -0500

    finished projects

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Link, BrowserRouter } from 'react-route
 import Home from './pages/Home'
 import Header from './links'
 import Blog from './blog'
+import Projects from './Projects'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <div className="App">
         <Route exact path='/' component={Home} />
         <Route exact path='/blog' component={Blog} />
+        <Route exact path='/projects' component={Projects} />
       </div>
     </Router>
   );
--- src/Projects.js
+++ src/Projects.js
@@ -5,7 +5,12 @@ function Projects() {
     return(
         <div>
             <h1>This is Projects page</h1>
-            <img src='' />
+            <h2>This is my projects 2 that used the Fbi most wanted api and the website i created consist of a social media platform
+                for bounty hunters to talk to eachothers
+            </h2>
+            <img src='https://cdn.discordapp.com/attachments/810662478664171524/810662867439321118/Screen_Shot_2021-02-14_at_2.23.06_AM.png' width='800px' height='500px' />
+            <h2>This project consisted of the player shooting zombies with the space bar and surviving an onslaut of 25 zombies spanwing randomly along the y axis</h2>
+            <img src='https://cdn.discordapp.com/attachments/810662478664171524/810664848135684096/Screen_Shot_2021-02-14_at_7.12.16_PM.png' width='800px' height='500px' />
         </div>
     )
 }
