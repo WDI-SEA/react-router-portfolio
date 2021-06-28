35000e036ad80970499a82ae73099b73a57ee316
weston-bailey <wdbailey87@gmail.com>
2021-06-27 18:00:42 -0700

    mvp

---
+++ src/components/About.jsx
@@ -0,0 +1,15 @@
+export default function About() {
+  return (
+    <div>
+      <h3>all about me:</h3>
+
+      <h5>Im p much the best 😎</h5>
+
+      <p>I make cool stuff with the codes 🎉</p>
+
+      <img src="http://placekitten.com/200/300"  alt="meowzers" />
+      
+      <p>look at the cat!</p>
+    </div>
+  )
+}
---
+++ src/components/Homepage.jsx
@@ -0,0 +1,9 @@
+export default function Homepage() {
+  return (
+    <div>
+      <h1>Hi 👋 welcome to my portfolio</h1>
+
+
+    </div>
+  )
+}
