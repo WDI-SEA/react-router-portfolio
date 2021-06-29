e6acb733108bccc78c8e6b8aaf340d91643b5058
nesbita <ariananesbit@gmail.com>
2021-06-29 10:50:19 -0600

    image added but not rendering

--- react-router-portfolio/src/Projects.jsx
+++ react-router-portfolio/src/Projects.jsx
@@ -1,9 +1,11 @@
+<img src='public/download.jpg'/>
 const Projects = (props) => {
     return(
         <div>
             This is the projects page.
         </div>
-    )
+ )
+ 
 }
 
 export default Projects;
