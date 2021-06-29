f21b83ae0fbb0ef027546a84a1c8cdb29aade0f0
Consecratio <jcasique@gmail.com>
2021-06-29 09:26:53 -0700

    added styling to carousel

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,6 @@
 // import css
 import './Projects.css'
+import Carousel from 'react-bootstrap/Carousel'
 
 // import images
 import spellbook from '../images/project_spellbook.jpg'
@@ -7,19 +8,36 @@ import checkers from '../images/project_checkers.jpg'
 
 export default function Projects() {
     return (
-        <div>
-            <ul>
-                <li>
-                    <h6><a href="https://spellbook-project.herokuapp.com/">Spellbook</a></h6>
-                    <p><a href="https://github.com/Consecratio/project-2-spellbook">GitHub</a></p>
-                    <img src={spellbook} className="image" alt="spellbook app" />
-                </li>
-                <li>
-                    <h6><a href="https://consecratio.github.io/Checkers/">Checkers</a></h6>
-                    <p><a href="https://github.com/Consecratio/Checkers">GitHub</a></p>
-                    <img src={checkers} className="image" alt="checkers app" />
-                </li>
-            </ul>
-        </div>
+        <Carousel className="project-carousel mt-3" fade>
+            <Carousel.Item>
+                <img
+                className="d-block w-100"
+                src={spellbook}
+                alt="First slide"
+                />
+                <Carousel.Caption>
+                    <h3><a href="https://spellbook-project.herokuapp.com/">Spellbook</a></h3>
+                    <p>
+                        Full-stack application utilizing Node.js, Express, PostgreSQL, Sequelize, EJS, and Bootstrap.
+                        <a href="https://github.com/Consecratio/project-2-spellbook"> GitHub</a>
+                    </p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                className="d-block w-100"
+                src={checkers}
+                alt="Second slide"
+                />
+
+                <Carousel.Caption>
+                    <h3><a href="https://consecratio.github.io/Checkers/">Checkers</a></h3>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        <a href="https://github.com/Consecratio/Checkers"> GitHub</a>
+                    </p>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
     )
 }
