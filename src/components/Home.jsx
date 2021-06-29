be75f441361bfe0d716c9ac6f86af9c4e6c0bd31
Consecratio <jcasique@gmail.com>
2021-06-29 09:41:10 -0700

    added some text to the home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,8 @@
 export default function Home() {
     return (
         <div>
-            Hello from Home
+            <h2>Home</h2>
+            <p>Welcome to my portoflio! Please take a look at my projects if you have a chance, and check out what I'm interested now in my Blog. If you'd like to work with me head over to my About page so we can get in touch!</p>
         </div>
     )
 }
