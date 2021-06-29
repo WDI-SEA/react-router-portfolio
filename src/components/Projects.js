86cf2b8ba0e2dc9cdf56f4399a71a1bd42ec8b75
funbunch <shannan_bunch@hotmail.com>
2021-06-28 23:40:35 -0700

    Added imgs to portfolio

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,10 +3,10 @@ export default function Projects() {
     <>
       <h2>My projects</h2>
 
-      <ul className="mx-auto">
-        <li><a href="http://bunchofideas.com/dsc-five-reasons.html">DSC Landing pages</a></li>
-        <li><a href="http://bunchofideas.com/dsc-boogies.html">DSC Landing email</a></li>
-        <li><a href="http://bunchofideas.com/project-1.html">Fox Searchlight</a></li>
+      <ul>
+        <li className="nav-link"><a href="http://bunchofideas.com/dsc-five-reasons.html"><img src="/dsc-five-reasons-lg.jpg" target="_blank" width="500" alt="" />DSC Landing pages</a></li>
+        <li className="nav-link"><a href="http://bunchofideas.com/dsc-boogies.html"><img src="/boogies.png" target="_blank" width="500" alt="" />DSC Landing email</a></li>
+        <li className="nav-link"><a href="http://bunchofideas.com/project-1.html"><img src="/3-bb-lg.jpg" target="_blank" width="500" alt="" />Fox Searchlight</a></li>
       </ul>
     </>
 
