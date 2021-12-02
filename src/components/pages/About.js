addd6b85de368b695e9a32e2bfbca2b819207dce
galyverasi <galyverasi@gmail.com>
2021-12-02 15:03:53 -0700

    initial about

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class About extends Component {
+    render () {
+        return (
+            <>
+            <h1>Hi I'm Galyver!</h1>
+            <p>I'm a software engineer student.</p>
+            </>
+        )
+    }
+}
