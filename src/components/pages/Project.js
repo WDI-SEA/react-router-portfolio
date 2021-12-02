fe36273d9efb71ae74469071902c03114ac74720
paulinal3 <paulinal3@outlook.com>
2021-12-02 16:46:07 -0500

    components/pages created

---
+++ src/components/pages/Project.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Project extends Component {
+    render() {
+        return (
+            <div>
+                <h1>This is the project page</h1>
+            </div>
+        )
+    }
+}
