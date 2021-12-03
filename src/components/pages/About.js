47ef9330e24458720ec3061340938b2b61d9db93
hectorzaragoza <zaragoza09@gmail.com>
2021-12-03 08:03:33 -0600

    added about me text

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,7 +3,10 @@ import React, { Component } from 'react'
 export default class About extends Component {
     render() {
         return (
+            <>
             <h1>About Me: </h1>
-        )
+            <p>I’m Hector - a Full-stack software engineer (MERN) - happy to navigate the waters between coding up an application from scratch to collaborating across teams to identify and address pain points. I’m driven by socially-oriented missions looking to build products that add value to their customers’ lives and looking for the right team to make it happen.</p>
+            </>
+            )
     }
 }
