21b1ff02bd4c0d5f1e769abcd97c0b18e549f0bf
dinosaur96m <nccadmantinaos@gmail.com>
2021-12-02 20:46:08 -0500

    added bacon ipsum

--- src/components/partials/Header.js
+++ src/components/partials/Header.js
@@ -4,15 +4,15 @@ class Header extends Component {
     render() {
         return (
             <header>
-            <nav>
-              <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/blog">Blog</Link></li>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/projects">Projects</Link></li>
-              </ul>
-            </nav>
-          </header>
+                <nav>
+                    <ul>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/blog">Blog</Link></li>
+                        <li><Link to="/about">About</Link></li>
+                        <li><Link to="/projects">Projects</Link></li>
+                    </ul>
+                </nav>
+            </header>
         )
     }
 }
