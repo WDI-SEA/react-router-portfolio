fdee26189e76267be3f9b480e43ffa4d2687bdff
paulinal3 <paulinal3@outlook.com>
2021-12-02 22:01:00 -0500

    saving in case internet dies on me again

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,7 +4,7 @@ export default class About extends Component {
     render() {
         return (
             <div>
-                <h1>This is the about page</h1>
+                <h1>About Me</h1>
             </div>
         )
     }
