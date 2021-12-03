8a6d964be1b5235af6ae35f8089918ead488e1c5
paulinal3 <paulinal3@outlook.com>
2021-12-02 19:26:21 -0500

    created post array and stored in blogPost variable

---
+++ src/components/BlogPosts.js
@@ -0,0 +1,23 @@
+// create a variable that will hold an array of post objects
+// each object should have a title and body (both as strings)
+const blogPost = {
+    posts: [
+        {
+            'title': 'post 1',
+            'date': 'March 25, 2021',
+            'body': `Caerphilly mozzarella pecorino. Cheese and biscuits paneer brie swiss everyone loves goat rubber cheese cauliflower cheese. Blue castello the big cheese cheeseburger feta when the cheese comes out everybody's happy when the cheese comes out everybody's happy chalk and cheese roquefort. Cheddar.`
+        },
+        {
+            'title': 'post 2',
+            'date': 'December 3, 2019',
+            'body': `Cauliflower cheese squirty cheese when the cheese comes out everybody's happy. Dolcelatte edam croque monsieur say cheese emmental macaroni cheese emmental jarlsberg. Halloumi cheese slices lancashire bocconcini manchego parmesan fromage frais stilton. Smelly cheese cheese on toast cut the cheese everyone loves parmesan roquefort port-salut squirty cheese. Gouda feta cheesy grin cheddar.
+
+            Gouda camembert de normandie cheese strings. Goat mascarpone roquefort cottage cheese cauliflower cheese chalk and cheese blue castello swiss. Bavarian bergkase boursin melted cheese cheese strings fondue dolcelatte babybel melted cheese. Camembert de normandie manchego swiss jarlsberg blue castello airedale blue castello cheesy feet. Port-salut.`
+        },
+        {
+            'title': 'post 3',
+            'date': 'September 21, 2018',
+            'body': `Macaroni cheese cheese triangles cheddar. Everyone loves mozzarella stilton swiss brie cheese and biscuits melted cheese cow. Caerphilly croque monsieur macaroni cheese cheese on toast cheesy feet cut the cheese rubber cheese taleggio. Ricotta everyone loves pepper jack roquefort manchego.`
+        }
+    ]
+}
