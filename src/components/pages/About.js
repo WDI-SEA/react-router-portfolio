a4fe5db4119920cf00428d84acd54a0b76c19107
devinrbopp <devinrbopp@gmail.com>
2021-12-02 18:51:12 -0700

    download resume button added

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
+import resume from '../../pdfs/Bopp_Resume_Draft.pdf'
 
 export default class About extends Component {
     render() {
         return (
             <div>
                 <h1>About Devin</h1>
-            </div>
+                <a href={resume} download>Download My Resume</a>
+            </div> 
         )
     }
 }
