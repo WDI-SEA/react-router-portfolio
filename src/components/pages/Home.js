b2998de6954648533dee4b4b782a66cf0ddf78b9
Mackmiller <mackmiller19@gmail.com>
2021-12-07 22:16:05 -0500

    switched from class component to function

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,15 +1,14 @@
-import React, { Component } from "react";
+import React from "react";
 
-export default class Home extends Component {
+export default function Home() {
 
-    render () {
-        return (
-        <div >
-            <h1>Finn and Nina</h1>
-            <p>Nina and Finn just want to live their best lives.</p>
-            <p>I'm following their example by not taking the time to style this site.</p>
-            <img src="doggos.JPG" alt="doggos"></img>
-        </div>
-        )
-    }
+    return (
+    <div >
+        <h1>Finn and Nina</h1>
+        <p>Nina and Finn just want to live their best lives.</p>
+        <p>I'm following their example by not taking the time to style this site.</p>
+        <img src="doggos.JPG" alt="doggos"></img>
+    </div>
+    )
+   
 }
