0c92aa29a45b8fb85acb105b87cccb8aec925bfd
Mike Kohlberg <mkohlberg95@gmail.com>
2022-02-08 21:36:51 -0600

    edited About text. got all videos to autoplay

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,9 @@ const About = () => {
       <h2 className='section-title'>About Me</h2>
       <div className='about-container'>
         <div className='about-text'>
-          <p>Full stack software developer with experience in mechanical and electrical engineering and PLC programming. My career shift was driven by my passion and aptitude for coding and my wanting to make a lasting impact. Trying to get to Mars so if you need someone, please let me know.</p>
+          <p>Full stack software developer with experience in mechanical and electrical engineering and PLC programming. My career shift was driven by my passion and aptitude for coding and my desire to make a lasting impact. I am comfortable working with many different types of people in a wide range of settings. I look forward to tackling your toughest bugs.</p>
+          <br></br>
+          <br></br>
           <div className='links-container'>
             <a href='https://linkedin.com/in/michaelkohlberg'>LinkedIn</a>
             <span> | </span>
--- src/components/ProjectsCarousel.js
+++ src/components/ProjectsCarousel.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import PokeCloneVid3 from '../videos/PokeCloneVid3.mov'
 import AlgoViewsVid1 from '../videos/AlgoViewsVid1.mov'
 import OoIHaveThatVid1 from '../videos/OoIHaveThatVid1.mov'
@@ -7,7 +7,20 @@ import KCMOMoversVid1 from '../videos/KCMOMoversVid1.mov'
 
 export default function ProjectsCarousel(){
 
+  // setTimeout required to get carousel.js to work
+  // think it's to do with manipulating elements not yet created
   setTimeout(()=>{require('../vanillaJS/carousel')},10)
+  // state variable required to get some videos to autoPlay
+  // not sure why
+  const [autoPlayPokeClone, setAutoPlayPokeClone] = useState(false)
+  const [autoPlayKCMOMovers, setAutoPlayKCMOMovers] = useState(false)
+  useEffect(()=>{
+    setAutoPlayPokeClone(true)
+  },[])
+  useEffect(()=>{
+    setTimeout(()=>setAutoPlayKCMOMovers(true),5)
+  },[autoPlayPokeClone])
+
   return (
     <div id="projects__container">
 
@@ -21,15 +34,10 @@ export default function ProjectsCarousel(){
           <ul className="carousel__track">
             <li className="carousel__slide current-slide" style={{left: "0px"}}>
               <div className="carousel__text__container">
-                {/* <h3 className="carousel__text__author">Harry Randy B.</h3>
-                <br/>
-                <h2 className="carousel__text">
-                  "These men were very professional, courteous, and diligent at their task through completion. I have used several people in different states through the years. I would certainly rate these as the best."
-                </h2> */}
                 <div>
                   <h2 className='project-title '>PokeClone</h2>
                   <p className='project-description carousel__text'>Roam a field of 'tall grass' and survive as many wild Pokemon encounters as you can. Search your favorite Pokemon or encounter them at random. Prepare yourself for the nostalgia that awaits.</p>
-                  <video src={PokeCloneVid3} autoplay="true" controls="controls" width="600" height="300" />
+                  <video src={PokeCloneVid3} autoPlay={autoPlayPokeClone} loop muted  controls="controls" width="600" height="300" />
                   <a className='project-link' target="_blank" href='https://mgkdn9.github.io/PokeClone/'>Live Link</a>
                   <a className='project-github' target='_blank' href='https://github.com/mgkdn9/PokeClone'>Github Repo</a>
                 </div>
@@ -40,7 +48,7 @@ export default function ProjectsCarousel(){
                 <div>
                   <h2 className='project-title'>AlgoViews</h2>
                   <p className='project-description'>Brush up on some common sorting algorithms and see them animated on screen. Then test your knowledge when you feel up to it.</p>
-                  <video src={AlgoViewsVid1} autoplay="true" controls="controls" width="600" height="300" />
+                  <video src={AlgoViewsVid1} autoPlay loop muted controls="controls" width="600" height="300" />
                   <a className='project-link' target="_blank" href='https://algo-views-client.herokuapp.com/'>Live Link!</a>
                   <a className='project-github' target='_blank' href='https://github.com/mgkdn9/sorting_algorithm_visualizer'>Github Repo</a>
                 </div>
@@ -51,7 +59,7 @@ export default function ProjectsCarousel(){
                 <div>
                   <h2 className='project-title'>OoIHaveThat</h2>
                   <p className='project-description'>Peer to peer tool sharing platform. Broadcast to users near you whenever you need a tool or any kind of service. Also see requests from users nearby and help a neighbor out by renting them a tool while earning some money off all the tools you own.</p>
-                  <video src={OoIHaveThatVid1} autoplay="true" controls="controls" width="600" height="300" />
+                  <video src={OoIHaveThatVid1} autoPlay loop muted controls="controls" width="600" height="300" />
                   <a className='project-link' target="_blank" href='https://ooihavethat.herokuapp.com/'>Live Link!</a>
                   <a className='project-github' target='_blank' href='https://github.com/mgkdn9/OoIHaveThat'>Github Repo</a>
                 </div>
@@ -62,7 +70,7 @@ export default function ProjectsCarousel(){
                 <div>
                   <h2 className='project-title'>Reart</h2>
                   <p className='project-description'>A subscription service delivers artwork right to your door. Tailored to your unique taste in art, season after season.</p>
-                  <video src={ReartVid1} autoplay="true" controls="controls" width="600" height="300" />
+                  <video src={ReartVid1} autoPlay loop muted controls="controls" width="600" height="300" />
                   <a className='project-link' target="_blank" href='https://reart.herokuapp.com/'>Live Link!</a>
                   <a className='project-github' target='_blank' href='https://github.com/mgkdn9/Re-Art-Client'>Github Repo</a>
                 </div>
@@ -73,7 +81,7 @@ export default function ProjectsCarousel(){
                 <div>
                   <h2 className='project-title'>KCMOMovers Website</h2>
                   <p className='project-description'>Personal company website for marketing purposes as well as coding practice.</p>
-                  <video src={KCMOMoversVid1} autoplay="true" controls="controls" width="600" height="300" />
+                  <video src={KCMOMoversVid1} autoPlay={autoPlayKCMOMovers} loop muted controls="controls" width="600" height="300" />
                   <a className='project-link' target="_blank" href='https://KCMOMovers.com/'>Live Link!</a>
                   <a className='project-github' target='_blank' href='https://github.com/mgkdn9/KCMOMoversWebsite'>Github Repo</a>
                 </div>
