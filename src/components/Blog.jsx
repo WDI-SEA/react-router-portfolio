7accd14260fa163b5ccde77cfd6a6f34ecf78063
ApexBortz <nicholas@georgetownframeshoppe.com>
2022-06-15 23:21:04 -0700

    Deliverable Complete

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,8 +1,22 @@
 
-const Blog = () => {
+const Blog = (props) => {
+
+    const post = props.posts.map((post, i) => {
+        const { title, date, body } = post
+
+        return (
+            <ul className='Blogpost' key={`post${i}`}>
+                <h3>{title}</h3>
+                <h4>{date}</h4>
+                <p>{body}</p>
+            </ul>
+        )
+    })
+
     return (
         <div>
-            
+            <h1> Upcoming Warhol Exhibitions: </h1>
+            {post}
         </div>
     );
 }
