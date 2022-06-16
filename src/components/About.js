9744082e8d084c87e42cf97f10b17c61cdef2765
Emily-Herndon <emilyvherndon@gmail.com>
2022-06-16 09:40:37 -0700

    added a resume download

--- src/components/About.js
+++ src/components/About.js
@@ -6,6 +6,7 @@ export default function About() {
         <h1>About Me</h1>
         <img src='./images/Opal-gremlin.jpeg' alt="cat mid-transformation into gremlin form" />
         <p>My name is Opal. I am almost 3 years old. The humans have deemed me the "Chief Stink Officer" of the apartment. It is a role I take very seriously. If they tell you not to give me cheese, don't listen to them, I love cheese.</p>
+        <a href="./images/Opals-resume.pdf" download>Download My Resume!</a>
     </div>
   )
 }
