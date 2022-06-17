01f4644e69a4ef724e01f46dd0a0b2218d6d9528
marzo20 <myjune91@gmail.com>
2022-06-16 20:55:16 -0700

    bonus

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useState } from 'react'
 import Homepage from './components/pages/Hompage'
 import Blog from './components/pages/Blog'
 import About from './components/pages/About';
@@ -7,7 +8,7 @@ import Header from './components/partials/Header';
 import './App.css';
 
 function App() {
-  const blog = [
+  const blogs = [
     {
      title: 'Project 3 coming' , 
      body: 'I feel like I just finished my project2, but project 3 is coming next week.'
@@ -17,13 +18,29 @@ function App() {
       body: 'At the beginning of unit 3, react seems so easy. As I learn more and more about react, it seems more complicated than Project 2.'
     }
   ]
+  const [blog, setBlog] = useState([...blogs])
+  const [post, setPost] = useState({})
+  const handleChange = (e) => {
+    console.log(e.target.name, e.target.value)
+    setPost({...post, [e.target.name]: e.target.value})
+    
+  } 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setBlog([...blog, post])
+    setPost({title: '' , body: ''})
+  }
   return (
     <BrowserRouter>
       <Header />
       <Routes>
         <Route path='/' element={<Homepage />} />
         <Route path='/blog' element={<Blog  
-        post={blog} />} />
+        posts={blog} 
+        handleChange={handleChange}
+        handleSubmit={handleSubmit}
+        newPost={post}
+        />} />
         <Route path='/about' element={<About />} />
         <Route path='/projects' element={<Projects />} />
       </Routes>
--- src/components/pages/Blog.js
+++ src/components/pages/Blog.js
@@ -1,5 +1,5 @@
 function Blog(props) {
-    const post = props.post.map((post, i) => {
+    const post = props.posts.map((post, i) => {
         return <div key={`post${i}`}>
             <h2>Title: {post.title}</h2>
             <p>- {post.body}</p>
@@ -9,6 +9,15 @@ function Blog(props) {
         <>
             <h2>Blog</h2>
             {post}
+            <form onSubmit={props.handleSubmit}>
+                <label htmlFor="title">Title: </label>
+                <input 
+                type="text" name="title" value={props.newPost.title} onChange={props.handleChange}/>
+                <label htmlFor="body">Body: </label>
+                <input type="text" name="body" value={props.newPost.body} onChange={props.handleChange}/>
+
+                <button type="submit">Post</button>
+            </form>
         </>
     );
 }
