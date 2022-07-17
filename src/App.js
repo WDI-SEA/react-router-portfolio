6fc71497ce909a6b5731e1fb81bf11087b3faab1
Mike Kohlberg <mkohlberg95@gmail.com>
2022-07-16 20:41:17 -0400

    got Bootstrap going. installed yarn and ran 'yarn add bootstrap react-bootstrap'

--- src/App.js
+++ src/App.js
@@ -1,31 +1,34 @@
 // External Resources
-import React from 'react'
+import React from "react";
+import Button from "react-bootstrap/Button";
 // Internal Resources
-import './App.css';
-import Resume from './Resume-MikeKohlberg-2022102.pdf'
-import ProjectsCarousel from './components/ProjectsCarousel'
-import Skills from './components/Skills'
-import About from './components/About'
+import "./App.css";
+import Resume from "./Resume-MikeKohlberg-2022102.pdf";
+import ProjectsCarousel from "./components/ProjectsCarousel";
+import Skills from "./components/Skills";
+import About from "./components/About";
 
 function App() {
-
   return (
     <div>
-      <div className='header-container'>
-        <a href={Resume} download>Download Resume</a>
+      <div className="header-container">
+        <a href={Resume} download>
+          Download Resume
+        </a>
+        <Button id="callToActionBtn" variant="primary" size="lg">Email Me</Button>
         <header>
           <h1>MICHAEL KOHLBERG</h1>
           <h2>Software Engineer</h2>
         </header>
       </div>
       <main>
-        <h2 className='section-title'>Projects</h2>
-        <ProjectsCarousel/>
-        <Skills/>
-        <About/>
+        <h2 className="section-title">Projects</h2>
+        <ProjectsCarousel />
+        <Skills />
+        <About />
       </main>
     </div>
-  )
+  );
 }
 
 export default App;
