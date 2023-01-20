7551f877a7ce391c597f253b7eeaab3be5a80a05
fmher <feumatthewher2@gmail.com>
2023-01-20 10:08:15 -0800

    attemp bounses

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -1,19 +1,30 @@
-
+import { useState } from 'react'
 
 
 export default function Blog(props) {
+    // set state of individual post
+    const [posts, setPosts] = useState(props.posts)
+
+    // use helper method to find specific post by index, then splice new post
+    // where old post used to be
+    function replacePost() {
+        
+    }
+
+
     return (
       <div>
-        {props.posts.map((post, i) => {
-            return (
-                <div>
-                    <h1><u>Blog</u></h1>
+            <h1><u>Blog</u></h1>
+            
+            {props.posts.map((post, i) => {
+                return (
+                    <div key={`${i}`}>
 
-                    <h1>{post.title}</h1>
-                    <p>{post.body}</p>
-                </div>
-            )
-        })}
+                        <h1>{post.title}</h1>
+                        <p>{post.body}</p>
+                    </div>
+                )
+            })}
       </div>
     )
 }
