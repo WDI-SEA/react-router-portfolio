261b99128d87688ba21018909dcadaf600ab6ab8
awellsbiz <awellsbiz@gmail.com>
2023-04-26 08:11:59 -0700

    Css applied

--- src/components/pages/Blog.jsx
+++ src/components/pages/Blog.jsx
@@ -1,22 +1,26 @@
 
 export default function Blog(props) {
    const listPost= props.allPost.map((post) => {
-        return <li>
+        return <div class="p-3" key={`title - ${post.title}`}>
                     <h4>{post.title}</h4>
                     <h5>{post.author}</h5>
                     <p>{post.body}</p>
-                </li>
+                </div>
     })
     return(
-        <div>
+        <div class="container px-4 text-center">
+            <div class="row gx-5">
+    <div class="col">
             <h1>Blog</h1>
-            <ul>
+        
                 {listPost}
-            </ul>
+            
             <h1>create post here</h1>
             <form>
                 
             </form>
+        </div>
+        </div>
         </div>
     )
 }
