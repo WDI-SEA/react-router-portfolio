f1d7c6ef992b35bd07dea01f353f46cbea480ab6
dxk0ta <dxkotacamps@gmail.com>
2023-05-10 15:32:45 -0700

    added background img

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,9 +1,10 @@
 import "./Home.css"
+import homebackground from "../assets/homebackground.jpg"
 
 export default function Home() {
     return (
         <>
-            <div className="home-title">
+            <div className="home-background">
                 <h1>Dakota Campbell</h1>
                 <p>Welcome to my personal coding blog.</p>
             </div >
