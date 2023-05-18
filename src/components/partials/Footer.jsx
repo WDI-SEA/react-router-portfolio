eb6ce50b6d55107f0435480846d77805a6f8b7ef
dxk0ta <dxkotacamps@gmail.com>
2023-05-17 23:02:22 -0700

    added linkedin

--- src/components/partials/Footer.jsx
+++ src/components/partials/Footer.jsx
@@ -1,5 +1,6 @@
 import "./Footer.css"
 import githubicon from "../assets/githubicon.jpg"
+import linkedinicon from "../assets/linkedinbtn.png"
 
 export default function Footer() {
 
@@ -8,12 +9,22 @@ export default function Footer() {
         <>
             <div className="footer-container">
                 <div className="footer-discription">
-                    Dakota Campbell's Github
+                    Dakotas Github
                 </div>
 
                 <div className="githubicon">
                     <a href="https://github.com/Dxk0ta" target="_blank" rel="noopener noreferrer">
                         <img src={githubicon} alt="github icon" />
+                    </a>
+                </div>
+
+                <div className="footer-discription2">
+                    Dakotas LinkedIn
+                </div>
+
+                <div className="linkedinicon">
+                    <a href="https://www.linkedin.com/in/dxk0ta/" target="_blank" rel="noopener noreferrer">
+                        <img src={linkedinicon} alt="linkedin icon" />
                     </a>
                 </div>
             </div>
